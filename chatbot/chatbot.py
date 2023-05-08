b53233b97f026add599264a150bfd6d4e8f1a311
Andrew Kant <akantunc@gmail.com>
2023-05-08 12:51:45 -0400

    add: chatbot chat/response functionality

--- chatbot/chatbot.py
+++ chatbot/chatbot.py
@@ -4,16 +4,20 @@ from dotenv import dotenv_values
 config = dotenv_values(".env")
 openai.api_key = config["OPENAI_API_KEY"]
 
-
+messages = []
 
 while True:
     try:
         user_input = input("You: ")
+        messages.append({"role": "user", "content": user_input})
         response = openai.ChatCompletion.create(
             model="gpt-3.5-turbo",
-            messages= [{"role": "user", "content": user_input}]
+            messages= messages
         )
-        print(response["choices"][0]["message"]["content"])
+        messages.append(response["choices"][0]["message"].to_dict())
+        # print(response["choices"][0]["message"]["content"])
+        print("Assistant: ", response["choices"][0]["message"]["content"])
+        # print("All Messages", messages)
     except KeyboardInterrupt: 
         print("Exiting...")
         break
